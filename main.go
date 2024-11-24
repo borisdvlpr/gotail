d86f9c246e8a6d22fc2017a87b0375f88ca12b87
borisdvlpr <eduffmiranda@hotmail.com>
2024-11-24 20:41:01 +0100

    feat: implement findUserData function to find user-data file
    This function finds the user-data file, which is going to be edited with Tailscale configuration

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
+	"io/fs"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"runtime"
 	"slices"
 	"strings"
 	"syscall"
@@ -44,9 +47,96 @@ func lsblk() (LsblkOutput, error) {
 		return LsblkOutput{}, fmt.Errorf("lsblk parsing: %w", err)
 	}
 
-	fmt.Println(lsblk)
-
 	return lsblk, nil
+}
+
+func getFilePath(rootDir string, fileName string) (string, error) {
+	var filePath string
+
+	err := filepath.WalkDir(rootDir, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !d.IsDir() && d.Name() == fileName {
+			filePath = path
+			return fs.SkipDir
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return "", fmt.Errorf("%w", err)
+	}
+
+	return filePath, nil
+}
+
+func searchMountpoints(mountpoints []string) (string, error) {
+	for _, mountpoint := range mountpoints {
+		if mountpoint != "" && !strings.Contains(mountpoint, "/snap") {
+			filePath, err := getFilePath(mountpoint, "user-data")
+			if err != nil {
+				return "", fmt.Errorf("%w", err)
+			}
+
+			if filePath != "" {
+				return filePath, nil
+			}
+		}
+	}
+
+	return "", nil
+}
+
+func findUserData() (string, error) {
+	var filePath string
+	var err error
+
+	if runtime.GOOS == "darwin" {
+		filePath, err = getFilePath("/Volumes", "user-data")
+		if err != nil {
+			return "", fmt.Errorf("%w", err)
+		}
+	}
+
+	if runtime.GOOS == "linux" {
+		devices, err := lsblk()
+		if err != nil {
+			return "", fmt.Errorf("%w", err)
+		}
+
+		for _, device := range devices.Blockdevices {
+			if device.Mountpoints != nil {
+				filePath, err = searchMountpoints(device.Mountpoints)
+				if err != nil {
+					return "", fmt.Errorf("%w", err)
+				}
+
+				if filePath != "" {
+					return filePath, nil
+				}
+			}
+
+			if device.Children != nil {
+				for _, child := range device.Children {
+					if child.Mountpoints != nil {
+						filePath, err = searchMountpoints(child.Mountpoints)
+						if err != nil {
+							return "", fmt.Errorf("%w", err)
+						}
+
+						if filePath != "" {
+							return filePath, nil
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return "", nil
 }
 
 func promptUser(prompt string, allowedReplies []string) (string, error) {
@@ -100,4 +190,16 @@ func main() {
 	if err := checkRoot(); err != nil {
 		handleError(err)
 	}
+
+	filePath, err := findUserData()
+	if err != nil {
+		handleError(err)
+	}
+
+	if filePath == "" {
+		fmt.Println("Could not find user-data file. Please try removing your SD card and re-inserting it.")
+		os.Exit(1)
+	}
+
+	fmt.Printf("Found user-data file at %s.\n", filePath)
 }
