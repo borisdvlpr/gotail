9613cf37f860068491d387341d53778a2437e156
borisdvlpr <eduffmiranda@hotmail.com>
2025-01-27 00:19:47 +0100

    test: add initial test for promptUser

---
+++ main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestPromptUser(t *testing.T) {
+	defaultStdin := os.Stdin
+	defer func() { os.Stdin = defaultStdin }()
+
+	r, w, _ := os.Pipe()
+	os.Stdin = r
+
+	go func() {
+		_, err := w.WriteString("y\n")
+		if err != nil {
+			t.Errorf("%v", err)
+		}
+
+		err = w.Close()
+		if err != nil {
+			t.Errorf("%v", err)
+		}
+	}()
+
+	answer, err := promptUser("Would you like this device to be an exit node?", []string{"y", "n"})
+	if err != nil {
+		t.Errorf("promptUser() error = %v, wantErr %v", err, false)
+	}
+
+	if answer != "y" {
+		t.Errorf("promptUser() = %v, want %v", answer, "y")
+	}
+}
