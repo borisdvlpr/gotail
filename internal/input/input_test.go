b10cd0e963afd56e5ee3b4aa0680a8124666314b
borisdvlpr <eduffmiranda@hotmail.com>
2025-02-03 19:15:38 +0100

    test: change input test setup to test multiple scenarios
    In this commit, the input_test.go test suite implements table driven tests, to allow for multiple test scenarios.

--- internal/input/input_test.go
+++ internal/input/input_test.go
@@ -1,35 +1,52 @@
 package input
 
 import (
+	"fmt"
 	"os"
 	"testing"
 )
+
+type PromptUserTestCase struct {
+	id             string
+	input          string
+	allowedInputs  []string
+	expectedAnswer string
+}
 
 func TestPromptUser(t *testing.T) {
 	defaultStdin := os.Stdin
 	defer func() { os.Stdin = defaultStdin }()
 
-	r, w, _ := os.Pipe()
-	os.Stdin = r
-
-	go func() {
-		_, err := w.WriteString("y\n")
-		if err != nil {
-			t.Errorf("%v", err)
-		}
-
-		err = w.Close()
-		if err != nil {
-			t.Errorf("%v", err)
-		}
-	}()
-
-	answer, err := PromptUser("Would you like this device to be an exit node?", []string{"y", "n"})
-	if err != nil {
-		t.Errorf("promptUser() error = %v, wantErr %v", err, nil)
+	testCases := []PromptUserTestCase{
+		{"case01", "y\n", []string{"y", "n"}, "y"},
+		{"case02", "tskey_test_1234_5678\n", nil, "tskey_test_1234_5678"},
 	}
 
-	if answer != "y" {
-		t.Errorf("promptUser() = %v, want %v", answer, "y")
+	for _, tc := range testCases {
+		r, w, _ := os.Pipe()
+		os.Stdin = r
+
+		go func() {
+			_, err := w.WriteString(tc.input)
+			if err != nil {
+				t.Errorf("%v", err)
+			}
+
+			err = w.Close()
+			if err != nil {
+				t.Errorf("%v", err)
+			}
+		}()
+
+		answer, err := PromptUser(tc.id, tc.allowedInputs)
+		fmt.Println("")
+
+		if err != nil {
+			t.Errorf("promptUser() error = %v, wantErr %v", err, nil)
+		}
+
+		if answer != tc.expectedAnswer {
+			t.Errorf("promptUser() = %v, want %v", answer, tc.expectedAnswer)
+		}
 	}
 }
