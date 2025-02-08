14e016c2c7de4356289cc67cdcba29db3e1e10e4
borisdvlpr <eduffmiranda@hotmail.com>
2025-02-08 18:53:59 +0100

    docs: add imcomplete comment description for ValidateSubnets function

--- internal/input/validation.go
+++ internal/input/validation.go
@@ -8,7 +8,10 @@ import (
 	ierror "github.com/borisdvlpr/gotail/internal/error"
 )
 
-// ValidateSubnets validates
+// ValidateSubnets validates a comma-separated list of subnets.
+// Each subnet must be in CIDR notation. If any subnet is not in
+// the correct format, an error is returned indicating the invalid
+// subnet and a status code of 1.
 func ValidateSubnets(subnets string) error {
 	for _, subnet := range strings.Split(subnets, ",") {
 		if _, _, err := net.ParseCIDR(subnet); err != nil {
