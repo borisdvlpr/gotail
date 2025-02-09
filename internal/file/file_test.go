5933d3b18982c99c8111d6094d656cdb9189884b
borisdvlpr <eduffmiranda@hotmail.com>
2025-02-09 17:06:48 +0100

    test: add test to GetFilePath function

---
+++ internal/file/file_test.go
@@ -0,0 +1,46 @@
+package file
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+type GetFilePathTestCase struct {
+	id           string
+	file         string
+	expectedPath string
+}
+
+func TestGetFilePath(t *testing.T) {
+	testCases := []GetFilePathTestCase{
+		{id: "case_01", file: "user-data", expectedPath: "file-test-dir/user-data"},
+		{id: "case_02", file: "", expectedPath: ""},
+	}
+
+	for _, tc := range testCases {
+		tempDir := filepath.Join(".", "file-test-dir")
+		err := os.MkdirAll(tempDir, 0755)
+		if err != nil {
+			t.Fatalf("Failed to create test dir: %v", err)
+		}
+
+		testFilePath := filepath.Join(tempDir, tc.file)
+		if tc.file != "" {
+			if _, err := os.Create(testFilePath); err != nil {
+				t.Fatalf("Failed to create test file: %v", err)
+			}
+		}
+
+		path, err := GetFilePath(tempDir, "user-data")
+		if err != nil {
+			t.Errorf("Unexpected error: %v", err)
+		}
+
+		if path != tc.expectedPath {
+			t.Errorf("%v: GetFilePath() path = %v, wantPath %v", tc.id, err, tc.expectedPath)
+		}
+
+		os.RemoveAll(tempDir)
+	}
+}
