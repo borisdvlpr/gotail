7041eec8f5f1414de8825646ba08af538c80a335
borisdvlpr <eduffmiranda@hotmail.com>
2025-05-13 19:30:02 +0200

    refactor(config): change ExitNode and SubnetRouter types from bool to string

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,9 +1,27 @@
 package config
 
+import "errors"
+
 type Config struct {
-	ExitNode     bool   `yaml:"exit_node"`
-	SubnetRouter bool   `yaml:"subnet_router"`
+	ExitNode     string `yaml:"exit_node"`
+	SubnetRouter string `yaml:"subnet_router"`
 	Subnets      string `yaml:"subnets"`
 	Hostname     string `yaml:"hostname"`
 	AuthKey      string `yaml:"auth_key"`
+}
+
+func (c *Config) Validate() error {
+	if c.AuthKey == "" {
+		return errors.New("auth_key is required")
+	}
+
+	if c.SubnetRouter == "y" && c.Subnets == "" {
+		return errors.New("subnets are required when subnet_router is enabled")
+	}
+
+	if c.Hostname == "" {
+		return errors.New("hostname is required")
+	}
+
+	return nil
 }
