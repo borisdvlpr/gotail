ef80d2ad974e658deb9ac8f066da36d758afcc6d
borisdvlpr <eduffmiranda@hotmail.com>
2026-05-25 23:48:27 +0100

    docs(file): add doc comment to unsupported utils NewSystemSearcher constructor

--- internal/file/unsupported_utils.go
+++ internal/file/unsupported_utils.go
@@ -4,6 +4,10 @@ package file
 
 import "github.com/spf13/afero"
 
+// NewSystemSearcher returns a SystemSearcher for unrecognised operating systems.
+// The embedded Searcher is set to nil as a sentinel value, indicating that drive
+// searching is not supported on the current platform. Callers should check for
+// this condition before invoking any search operations.
 func NewSystemSearcher(fsys afero.Fs) *SystemSearcher {
 	return &SystemSearcher{
 		Fsys:     fsys,
