6ccc896ed69c0598c8f951a2ba34f96aa9e35a4f
borisdvlpr <eduffmiranda@hotmail.com>
2026-07-09 23:41:19 +0100

    test(input): add test cases for ValidateTags method

--- internal/input/validation_test.go
+++ internal/input/validation_test.go
@@ -13,6 +13,13 @@ type ValidateSubnetsTestCase struct {
 	expectedError error
 }
 
+type ValidateTagsTestCase struct {
+	id             string
+	input          string
+	expectedOutput string
+	expectError    bool
+}
+
 func TestValidateSubnet(t *testing.T) {
 	testCases := []ValidateSubnetsTestCase{
 		{
@@ -81,6 +88,92 @@ func TestValidateSubnet(t *testing.T) {
 		err := ValidateSubnets(tc.subnet)
 		if !errors.Is(err, tc.expectedError) {
 			t.Errorf("%v: ValidateSubnets() error = %q, wantErr %q", tc.id, err, tc.expectedError)
+		}
+	}
+}
+
+func TestValidateTags(t *testing.T) {
+	testCases := []ValidateTagsTestCase{
+		{
+			id:             "single_tag",
+			input:          "server",
+			expectedOutput: "tag:server",
+			expectError:    false,
+		},
+		{
+			id:             "multiple_tags",
+			input:          "server,prod",
+			expectedOutput: "tag:server,tag:prod",
+			expectError:    false,
+		},
+		{
+			id:             "hyphenated_name",
+			input:          "prod-2",
+			expectedOutput: "tag:prod-2",
+			expectError:    false},
+		{
+			id:             "uppercase_normalized",
+			input:          "Server",
+			expectedOutput: "tag:server",
+			expectError:    false},
+		{
+			id:             "whitespace_trimmed",
+			input:          " server , prod ",
+			expectedOutput: "tag:server,tag:prod",
+			expectError:    false},
+		{
+			id:          "prefix_included",
+			input:       "tag:server",
+			expectError: true},
+		{
+			id:          "space_in_name",
+			input:       "my server",
+			expectError: true},
+		{
+			id:          "underscore",
+			input:       "prod_env",
+			expectError: true},
+		{
+			id:          "special_char",
+			input:       "server!",
+			expectError: true},
+		{
+			id:          "trailing_comma",
+			input:       "server,",
+			expectError: true},
+		{
+			id:          "whitespace_only",
+			input:       "   ",
+			expectError: true},
+	}
+
+	for _, tc := range testCases {
+		output, err := ValidateTags(tc.input)
+
+		if tc.expectError {
+			if err == nil {
+				t.Errorf("%v: ValidateTags(%q) returned no error, expected one", tc.id, tc.input)
+				continue
+			}
+
+			var statusErr ierror.StatusError
+			if !errors.As(err, &statusErr) {
+				t.Errorf("%v: ValidateTags(%q) returned %T, expected ierr.StatusError", tc.id, tc.input, err)
+
+			} else if statusErr.StatusCode != 1 {
+				t.Errorf("%v: ValidateTags(%q) returned status code %d, expected 1", tc.id, tc.input, statusErr.StatusCode)
+			}
+
+			continue
+		}
+
+		if err != nil {
+			t.Errorf("%v: ValidateTags(%q) returned error %v, expected none", tc.id, tc.input, err)
+			continue
+		}
+
+		if output != tc.expectedOutput {
+			t.Errorf("%v: ValidateTags(%q) = %q, expected %q", tc.id, tc.input, output, tc.expectedOutput)
 		}
 	}
 }
